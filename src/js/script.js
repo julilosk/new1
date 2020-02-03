6d9c678fecda189aafaa718b521dbe65042364a6
julilosk <loskutovajm@mail.ru>
2020-02-03 13:57:52 +0700

    gray container

--- src/js/script.js
+++ src/js/script.js
@@ -31,3 +31,22 @@ $(document).ready(function(){
   });
 
 });
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.menu__block'),
+  menuItem = document.querySelectorAll('.menu__item'),
+  hamburger = document.querySelector('.hamburger');
+
+  hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu__block_active');
+  });
+
+  menuItem.forEach(item => {
+      item.addEventListener('click', () => {
+          hamburger.classList.toggle('hamburger_active');
+          menu.classList.toggle('menu__block_active');
+      });
+  });
+});
