aa60f7ad3bfa967214fd7a3640a2b443bd4810c4
julia <loskutovajm@mail.ru>
2020-02-09 21:04:43 +0700

    0902

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,12 +4,15 @@ $(document).ready(function(){
   
   //tabs
     
-  $('ul.toplist__tabs').on('click', 'li:not(.toplist__tab_active)', function() {
-      $(this)
-        .addClass('toplist__tab_active').siblings().removeClass('toplist__tab_active')
-        .closest('div.toplist').find('div.toplist__content').removeClass('toplist__content_active').eq($(this).index()).addClass('toplist__content_active');
-    });
+  // $('ul.toplist__tabs').on('click', 'li:not(.toplist__tab_active)', function() {
+  //     $(this)
+  //       .addClass('toplist__tab_active').siblings().removeClass('toplist__tab_active')
+  //       .closest('div.toplist').find('div.toplist__content').removeClass('toplist__content_active').eq($(this).index()).addClass('toplist__content_active');
+  // });
 
+
+  
+ 
     
   // activate wow.js - подключение скрипта для анимации
   new WOW().init(); 
@@ -49,4 +52,47 @@ window.addEventListener('DOMContentLoaded', () => {
           menu.classList.toggle('menu__block_active');
       });
   });
+});
+
+
+// tabs2
+
+$('.tabs-nav li').click(function(e) {
+  var a = $(this),
+      parent = a.parents('.tabs'),
+      nav = parent.children('.tabs-nav').children('li'),
+      box = parent.children('.tabs-box').children('div');
+ 
+  if (!a.hasClass('active')) {
+    a.addClass('active')
+      .siblings().removeClass('active');
+ 
+    box.eq(a.index()).addClass('active')
+      .siblings().removeClass('active');
+  }
+ 
+  e.preventDefault();
+});
+
+
+
+
+// tabs3
+
+
+$('.toplist__tabs li').click(function(e) {
+  var a = $(this),
+      parent = a.parents('.toplist'),
+      nav = parent.children('.toplist__tabs').children('li'),
+      box = parent.children('.toplist__box').children('div');
+ 
+  if (!a.hasClass('active')) {
+    a.addClass('active')
+      .siblings().removeClass('active');
+ 
+    box.eq(a.index()).addClass('active')
+      .siblings().removeClass('active');
+  }
+ 
+  e.preventDefault();
 });
