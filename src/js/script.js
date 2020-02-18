e9a948f73947e0c5371e4652e6af03b4e5522122
julia <loskutovajm@mail.ru>
2020-02-18 20:57:42 +0700

    1802

--- src/js/script.js
+++ src/js/script.js
@@ -63,6 +63,7 @@ document.querySelectorAll('.menu li a').forEach(function(el) {
     return false;
   });
 
+  
 
   $("#add").on("click", function() {
     $("<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>").appendTo("section");
@@ -88,6 +89,8 @@ window.addEventListener('DOMContentLoaded', () => {
       });
   });
 });
+
+
 
 
 // tabs nuber two
