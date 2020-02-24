8e4661ee591414e686da9b29b72721f42b3c939d
julia <loskutovajm@mail.ru>
2020-02-24 20:07:01 +0700

    2402

--- src/js/script.js
+++ src/js/script.js
@@ -77,9 +77,9 @@ $('ul.calendar__tabs').on('click', 'li:not(.calendar__tab_active)', function() {
 
   
 
-$("#add").on("click", function() {
-  $("<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>").appendTo("section");
-});
+// $("#add").on("click", function() {
+//   $("<p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>").appendTo("section");
+// });
 
 });
 
