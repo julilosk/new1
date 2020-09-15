0a8a0f3a03159da163f3e90195d5481ef4b5d675
Лоскутова Юлия Марсельевна <P22_LoskutovaYM@rosstat.local>
2020-09-15 16:46:23 +0700

    1509

--- src/js/script.js
+++ src/js/script.js
@@ -289,19 +289,40 @@ $(document).ready(function () {
 window.addEventListener('DOMContentLoaded', () => {
   const menu = document.querySelector('.menu__block'),
     menuItem = document.querySelectorAll('.menu__item'),
+    menuSubmenu = document.querySelectorAll('.menu__sub-menu'),
     hamburger = document.querySelector('.hamburger');
+
 
   hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger_active');
     menu.classList.toggle('menu__block_active');
   });
 
+
   menuItem.forEach(item => {
     item.addEventListener('click', () => {
       hamburger.classList.toggle('hamburger_active');
       menu.classList.toggle('menu__block_active');
     });
+
+
   });
+
+  menuSubmenu.forEach(item => {
+    item.addEventListener('click', () => {
+      hamburger.classList.toggle('hamburger_active');
+      menu.classList.toggle('menu__block_active');
+    });
+
+
+  });
+
+
+
+
+
+
+
 });
 
 
