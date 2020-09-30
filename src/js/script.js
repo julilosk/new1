46f550339f3bc0bd797c6625c6909e1b7d6dc22f
Лоскутова Юлия Марсельевна <P22_LoskutovaYM@rosstat.local>
2020-09-30 15:47:51 +0700

    3009

--- src/js/script.js
+++ src/js/script.js
@@ -280,28 +280,28 @@ $(document).ready(function () {
 
 
 
-  $('.owl-carousel').owlCarousel();
+//   $('.owl-carousel').owlCarousel();
 
 
-  $('#carouselOne').owlCarousel({
-    loop:true, //Зацикливаем слайдер
-    margin:50, //Отступ от элемента справа в 50px
-    nav:true, //Отключение навигации
-    autoplay:true, //Автозапуск слайдера
-    smartSpeed:1000, //Время движения слайда
-    autoplayTimeout:2000, //Время смены слайда
-    responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:4
-        }
-    }
-});
+//   $('#carouselOne').owlCarousel({
+//     loop:true, //Зацикливаем слайдер
+//     margin:50, //Отступ от элемента справа в 50px
+//     nav:true, //Отключение навигации
+//     autoplay:true, //Автозапуск слайдера
+//     smartSpeed:1000, //Время движения слайда
+//     autoplayTimeout:2000, //Время смены слайда
+//     responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:2
+//         },
+//         1000:{
+//             items:4
+//         }
+//     }
+// });
 
 
 
