34c5bf4bb936677e83f9a5b84b0b5105f226e629
Лоскутова Юлия Марсельевна <P22_LoskutovaYM@rosstat.local>
2021-02-08 16:10:18 +0700

    0802

--- dist/js/script.js
+++ dist/js/script.js
@@ -303,16 +303,126 @@ $(document).ready(function() {
     });
 
 
-    // carousel-photo для блока новости
-    $('.slider-photo').slick({
-        // speed: 1200,
-        adaptiveHeight: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../../icons/left.svg" alt="slide"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../../icons/right.svg" alt="slide"></button>'
-    });
+    
+  // // carousel-photo для блока новости
+
+    $(function() { 
+        // Card's slider
+          var $carousel = $('.slider-photo');
+        
+          $carousel
+            .slick({
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: true,
+              dots: true,
+              fade: true,
+              adaptiveHeight: true,
+              swipeToSlide: true,
+                //   asNavFor: '.slider-nav'
+                prevArrow: '<button type="button" class="slick-prev"><img src="../../icons/left.svg" alt="slide"></button>',
+                nextArrow: '<button type="button" class="slick-next"><img src="../../icons/right.svg" alt="slide"></button>',
+            })
+            .magnificPopup({
+              type: 'image',
+              delegate: 'a:not(.slick-cloned)',
+              closeOnContentClick: false,
+              tLoading: 'Загрузка...',
+              mainClass: 'mfp-zoom-in mfp-img-mobile',
+              image: {
+                verticalFit: true,
+                tError: '<a href="%url%">Фото #%curr%</a> не загрузилось.'
+              },
+              gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                tCounter: '<span class="mfp-counter">%curr% из %total%</span>', // markup of counte
+                preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+              },
+              zoom: {
+                enabled: true,
+                duration: 300
+              },
+              removalDelay: 300, //delay removal by X to allow out-animation
+              callbacks: {
+                open: function() {
+                  //overwrite default prev + next function. Add timeout for css3 crossfade animation
+                  $.magnificPopup.instance.next = function() {
+                    var self = this;
+                    self.wrap.removeClass('mfp-image-loaded');
+                    setTimeout(function() { $.magnificPopup.proto.next.call(self); }, 120);
+                  };
+                  $.magnificPopup.instance.prev = function() {
+                    var self = this;
+                    self.wrap.removeClass('mfp-image-loaded');
+                    setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
+                  };
+                  var current = $carousel.slick('slickCurrentSlide');
+                  $carousel.magnificPopup('goTo', current);
+                },
+                imageLoadComplete: function() {
+                  var self = this;
+                  setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
+                },
+                beforeClose: function() {
+                  $carousel.slick('slickGoTo', parseInt(this.index));
+                }
+              }
+            });
+        
+          
+          
+        });
+
+    // // carousel-photo для блока новости
+    // $('.slider-photo')
+    //     .slick({
+    //     // speed: 1200,
+    //     adaptiveHeight: true,
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     dots: true,
+    //     prevArrow: '<button type="button" class="slick-prev"><img src="../../icons/left.svg" alt="slide"></button>',
+    //     nextArrow: '<button type="button" class="slick-next"><img src="../../icons/right.svg" alt="slide"></button>',
+    //     swipeToSlide: true,
+    //     // infinite: false
+    // })
+    //    .magnificPopup({
+    //         delegate: 'a:not(.slick-cloned)',
+    //         type: 'image',
+    //         fixedContentPos: true,
+    //         closeOnBgClick: true,
+    //         alignTop: false,
+    //         tLoading: 'Загрузска #%curr%...',
+    //         mainClass: 'mfp-img-mobile',
+    //         gallery: {
+    //             enabled: true,
+    //             navigateByImgClick: true,
+    //             preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    //         }
+    //     // });
+    // });
+
+
+// function lightboxTrigger() {
+//     $('.slider-photo').magnificPopup({
+// 		delegate: 'a:not(.slick-cloned)',
+// 		type: 'image',
+//         fixedContentPos: true,
+//         closeOnBgClick: true,
+//         alignTop: false,
+// 		tLoading: 'Загрузска #%curr%...',
+// 		mainClass: 'mfp-img-mobile',
+// 		gallery: {
+// 			enabled: true,
+// 			navigateByImgClick: true,
+// 			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+// 		}
+// 	});
+// }
+// lightboxTrigger();
+
+
 
 
     // carousel-photo - карусель с эскизами
